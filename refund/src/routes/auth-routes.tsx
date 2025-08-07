873357b8ab0e0efa5b360113b6c0ed60b46f87eb
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-08-07 17:22:33 -0300

    Criando o AuthLayout - refund
    explorei como criar um layout padrão para as telas de sign-in e sign-up usando o Figma como referência. Mostrei como estruturar o componente AlfLayout.tsx, importando o logo e utilizando o Outlet para renderizar as rotas. Também abordei a aplicação de estilos com Tailwind CSS, como definir largura, altura e cores de fundo, além de implementar breakpoints para responsividade. O foco foi reaproveitar o design e garantir uma experiência visual consistente entre as páginas.

--- refund/src/routes/auth-routes.tsx
+++ refund/src/routes/auth-routes.tsx
@@ -1,12 +1,15 @@
 // rotas de autenticação
 import { Routes, Route } from "react-router";
 
+import { AuthLayout } from "../components/AuthLayout";
 import { SignIn } from "../pages/SignIn";
 
 export function AuthRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={<SignIn />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<AuthLayout />}>
+        <Route path="/" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 }
