fff67050b07f4b85874c71aacb2ea9c65970a1c8
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-08-08 23:46:06 -0300

    Criando Not Found - refund
    vamos abordar como lidar com rotas inexistentes em uma aplicação. Quando um usuário tenta acessar uma rota que não existe, em vez de uma tela em branco, vamos criar uma página de "não encontrado". Mostraremos como estruturar essa página com um título, um emoji e um link para retornar ao início. Além disso, faremos algumas estilizações para melhorar a aparência, utilizando classes para centralizar e formatar o texto. É uma maneira simples de melhorar a experiência do usuário!

--- refund/src/routes/AuthRoutes.tsx
+++ refund/src/routes/AuthRoutes.tsx
@@ -2,8 +2,10 @@
 import { Routes, Route } from "react-router";
 
 import { AuthLayout } from "../components/AuthLayout";
+
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
+import { NotFound } from "../pages/NotFound";
 
 export function AuthRoutes() {
   return (
@@ -12,6 +14,7 @@ export function AuthRoutes() {
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
