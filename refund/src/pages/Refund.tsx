c8d5a78d9fabb059a4890e0bb65ae720fa3127d0
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-08-09 23:12:49 -0300

    Criando Formulário - refund
     vamos focar na construção de um formulário para solicitações de reembolso. Começamos removendo elementos desnecessários e criando um cabeçalho com um título e uma descrição. Em seguida, importamos o componente de input para adicionar campos obrigatórios, como o nome da solicitação. Também aplicamos classes para estilizar o formulário, garantindo que ele se ajuste bem em diferentes tamanhos de tela. O objetivo é criar uma estrutura clara e funcional para o usuário.

--- refund/src/pages/Refund.tsx
+++ refund/src/pages/Refund.tsx
@@ -1,3 +1,12 @@
-export   function Refund() {
-    return <h1>Refund</h1>
+import { Input } from "../components/input";
+
+export function Refund() {
+  return <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]">
+    <header>
+        <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
+        <p className="text-sm text-gray-200 mt-2 mb-4">Dados da pessoa para solicitar reembolso.</p>
+
+        <Input required legend="Nome da solicitação"/>
+    </header>
+  </form>;
 }
