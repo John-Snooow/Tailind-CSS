7be2d3415fcaeecf4f6c9343675ff383a579eb75
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-08-10 16:49:28 -0300

    Populando select com Categorias
    vamos criar um arquivo essencial chamado categories.ts na pasta utils, onde organizamos recursos úteis para nossa aplicação. Importamos ícones de categorias e definimos um objeto com as categorias e seus respectivos ícones. Também mostramos como extrair apenas os nomes das categorias. Em seguida, utilizamos essas categorias para popular um componente Select, tornando-o dinâmico. Aprendemos a gerenciar o estado da categoria selecionada e a exibir corretamente as opções no Select.

--- refund/src/pages/Refund.tsx
+++ refund/src/pages/Refund.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/input";
 import { Select } from "../components/Select";
 
 export function Refund() {
-  return <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]">
-    <header>
-        <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
-        <p className="text-sm text-gray-200 mt-2 mb-4">Dados da pessoa para solicitar reembolso.</p>
+  const[category, setCategory] = useState("")
+  return (
+    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]">
+      <header>
+        <h1 className="text-xl font-bold text-gray-100">
+          Solicitação de reembolso
+        </h1>
+        <p className="text-sm text-gray-200 mt-2 mb-4">
+          Dados da pessoa para solicitar reembolso.
+        </p>
 
-        <Input required legend="Nome da solicitação"/>
-        <Select required legend="Categoria"/>
-    </header>
-  </form>;
+        <Input required legend="Nome da solicitação" />
+        <Select required
+         legend="Categoria"
+         value={category}
+         onChange={(e) => setCategory(e.target.value)}>
+          {CATEGORIES_KEYS.map((category) => (
+            <option key={category} value={category}>{CATEGORIES[category].name}</option>
+          ))}
+        </Select>
+      </header>
+    </form>
+  );
 }
