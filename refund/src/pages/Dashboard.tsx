d7fb171936571327168b5a468f24e9f97e504cdf
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-08-28 17:15:07 -0300

    Input de pesqisa - Refund
    vamos desenvolver um componente de input para pesquisa. Começamos importando o input e criando um formulário com um placeholder que sugere a busca pelo nome. Ajustamos o layout usando flexbox e definimos estilos como bordas e espaçamentos. Em seguida, implementamos um estado com useState para gerenciar o valor do input e simulamos uma busca. Também abordamos a prevenção do comportamento padrão do formulário ao submeter. Ao final, testamos a funcionalidade no console.

--- refund/src/pages/Dashboard.tsx
+++ refund/src/pages/Dashboard.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
+import { Input } from "../components/input"
+
 export function Dashboard() {
+    const [name, setName] = useState('')
+
+    function fetchRefunds(e: React.FormEvent) {
+        e.preventDefault()
+
+        console.log(name)
+    }
+
     return (
         <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
             <h1 className="text-gray-100 font-bold text-lg flex-1">Solicitações</h1>
+
+            <form
+            onSubmit={fetchRefunds}
+            className="flex flex-1 items-center justify-between pb-6 border-b-[1px]
+            border-gray-400 md:flex-row gap-2 mt-6"
+            >
+                <Input 
+                    placeholder="Pesquisar pelo nome"
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </form>
         </div>
     )
 }
