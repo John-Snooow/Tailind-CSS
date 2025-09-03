6b6010c1037d8ef291fcc423b084978b006379d9
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-09-03 17:12:48 -0300

    Botão para Visualizar Arquivo - Refund
    explorei como substituir um botão por um link que, ao ser clicado, abre uma nova página. Utilizei o site da Rocket como exemplo e implementei um sistema que verifica a existência de um parâmetro antes de decidir qual ação tomar. Também estilizei o componente, adicionei uma imagem e apliquei transições sutis. Demonstrei como a navegação entre rotas afeta a exibição dos elementos, destacando a diferença entre as páginas de upload e visualização de comprovantes.

--- refund/src/pages/Refund.tsx
+++ refund/src/pages/Refund.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router";
 
+import fileSvg from "../assets/file.svg";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 
 import { Input } from "../components/input";
@@ -16,21 +17,23 @@ export function Refund() {
   const [filename, setFilename] = useState<File | null>(null);
 
   const navigate = useNavigate();
-  const params = useParams <{id: string}>();
-
+  const params = useParams<{ id: string }>();
 
   function onSubmit(event: React.FormEvent) {
     event.preventDefault();
 
-    if(params.id) {
+    if (params.id) {
       return navigate(-1);
     }
 
-    console.log(name, amount, category, filename)
-    navigate("/confirm", { state: {fromSubmit: true }});
+    console.log(name, amount, category, filename);
+    navigate("/confirm", { state: { fromSubmit: true } });
   }
   return (
-    <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]">
+    <form
+      onSubmit={onSubmit}
+      className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w[512px]"
+    >
       <header>
         <h1 className="text-xl font-bold text-gray-100">
           Solicitação de reembolso
@@ -40,45 +43,56 @@ export function Refund() {
         </p>
       </header>
 
-      <Input 
-        required 
+      <Input
+        required
         legend="Nome da solicitação"
         value={name}
-        onChange={(e) => steName(e.target.value)} 
+        onChange={(e) => steName(e.target.value)}
         disabled={!!params.id}
+      />
+
+      <div className=" flex gap-4">
+        <Select
+          required
+          legend="Categoria"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          disabled={!!params.id}
+        >
+          {CATEGORIES_KEYS.map((category) => (
+            <option key={category} value={category}>
+              {CATEGORIES[category].name}
+            </option>
+          ))}
+        </Select>
+
+        <Input
+          legend="Valor"
+          required
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          disabled={!!params.id}
         />
+      </div>
 
-    <div className=" flex gap-4">
-      <Select
-        required
-        legend="Categoria"
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-        disabled={!!params.id}
-
-      >
-        {CATEGORIES_KEYS.map((category) => (
-          <option key={category} value={category}>
-            {CATEGORIES[category].name}
-          </option>
-        ))}
-      </Select>
-
-      <Input 
-        legend="Valor" 
-        required
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-        disabled={!!params.id}
+      {params.id ? (
+        <a
+          href="https://www.rocketseat.com.br"
+          target="_blank"
+          className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 hover:opacity-70 ease-linear"
+        >
+          <img src={fileSvg} alt="Ícone de arquivo" />
+          Abrir comprovante
+        </a>
+      ) : (
+        <Upload
+          fileName={filename && filename.name}
+          onChange={(e) => e.target.files && setFilename(e.target.files[0])}
         />
-    </div>
+      )}
 
-    <Upload
-      fileName={filename && filename.name}
-      onChange={(e) => e.target.files && setFilename(e.target.files[0])}
-    />
-    <Button type="submit" isloading={isLoading}>
-      {params.id ? "Voltar" : "Enviar"}
+      <Button type="submit" isloading={isLoading}>
+        {params.id ? "Voltar" : "Enviar"}
       </Button>
     </form>
   );
