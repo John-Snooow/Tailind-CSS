f71bab63f3d88a1c54660580007deb3bc1280d44
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-09-03 19:57:27 -0300

    componente de Loading - Refund
    Aqui eu criei uma página de carregamento para quando for Colocado e-mail e senha As informações vão ser redirecionados para uma API E enquanto isso Vai aparecer a página de carregar

--- refund/src/routes/index.tsx
+++ refund/src/routes/index.tsx
@@ -1,14 +1,21 @@
 // Para ver se um usuário está logado ou não .
 import { BrowserRouter } from "react-router";
 
+import { Loading } from "../components/Loading";
+
 import { AuthRoutes } from "./AuthRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 
+const isLoading = false; // Simulação de carregamento se eu colocar true, aparece o loading
+
 export function Routes() {
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <AuthRoutes />
     </BrowserRouter>
   );
 }
