2b8bc1a29b054c8de83bb1488552ce157aef0c66
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-09-11 20:02:56 -0300

    useActionState - refund
    Substitui o gerenciamento de formulários baseado em useState por useActionState e uma ação de formulário para melhorar o tratamento do estado. O formulário agora usa os atributos defaultValue e name para entradas, e o manipulador de envio é movido para uma função signIn assíncrona.

--- refund/src/pages/SignIn.tsx
+++ refund/src/pages/SignIn.tsx
@@ -1,44 +1,50 @@
+import { useActionState } from "react";
 
-import { useState } from "react"
-import { Input } from "../components/input"
-import { Button } from "../components/Button"
+import { Input } from "../components/input";
+import { Button } from "../components/Button";
 
 export function SignIn() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [isLoading, setIsLoading] = useState(false)
+  const [state, formAction, isLoading] = useActionState(signIn, {
+    email: "",
+    password: "",
+  });
 
-    function onSubmit(e: React.FormEvent) {
-        e.preventDefault()
+  async function signIn(prevState: any, formData: FormData) {
+    const email = formData.get("email");
+    const password = formData.get("password");
 
-        console.log(email, password)
-    }
-    return <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
-        <Input
-            required
-            legend="E-mail"
-            type="email"
-            placeholder="Digite seu e-mail"
-            onChange={(e) => setEmail(e.target.value)}
-        />
+    return { email, password };
+  }
+  return (
+    <form action={formAction} className="w-full flex flex-col gap-4">
+      <Input
+        name="email"
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="Digite seu e-mail"
+        defaultValue={String(state?.email)}
+      />
 
-        <Input
-            required
-            legend="Senha"
-            type="password"
-            placeholder="Digite sua senha"
-            onChange={(e) => setPassword(e.target.value)}
+      <Input
+        name="password"
+        required
+        legend="Senha"
+        type="password"
+        placeholder="Digite sua senha"
+        defaultValue={String(state?.password)}
+      />
 
-        />
+      <Button type="submit" isloading={isLoading}>
+        Entrar
+      </Button>
 
-            <Button type="submit" isloading={isLoading}>
-                Entrar
-            </Button>
-
-            <a 
-            href="/signup" className="text-sm fonf-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
-            >
-                Criar conta
-            </a>
+      <a
+        href="/signup"
+        className="text-sm fonf-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Criar conta
+      </a>
     </form>
+  );
 }
