b680e5773c969ce2ff65847019f99bbf3c9863cd
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-09-12 20:39:06 -0300

    Adicionar validação Zod ao formulário SignIn - refund
    Introduz a validação do esquema Zod para os campos de e-mail e senha na página SignIn. Exibe mensagens de erro de validação para o usuário e remove os valores de entrada padrão do formulário.

--- refund/src/pages/SignIn.tsx
+++ refund/src/pages/SignIn.tsx
@@ -1,19 +1,33 @@
 import { useActionState } from "react";
+import { z, ZodError }   from "zod";
 
 import { Input } from "../components/input";
 import { Button } from "../components/Button";
 
+const signInSchema = z.object({
+  email: z.email({message: "E-mail inválido"}),
+  password: z.string().trim().min(1, {message: "Informe a senha"}),
+})
+
 export function SignIn() {
-  const [state, formAction, isLoading] = useActionState(signIn, {
-    email: "",
-    password: "",
-  });
+  const [state, formAction, isLoading] = useActionState(signIn, null);
 
-  async function signIn(prevState: any, formData: FormData) {
-    const email = formData.get("email");
-    const password = formData.get("password");
+  async function signIn(_: any, formData: FormData) {
+    try{
+      const data = signInSchema.parse({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      })
 
-    return { email, password };
+      console.log(data)
+    }catch(error){
+      console.log(error)
+
+      if(error instanceof ZodError){
+        return { massage:error.issues[0].message}
+      }
+      return { message: "Erro ao fazer login, tente novamente mais tarde."}
+    }
   }
   return (
     <form action={formAction} className="w-full flex flex-col gap-4">
@@ -23,7 +37,6 @@ export function SignIn() {
         legend="E-mail"
         type="email"
         placeholder="Digite seu e-mail"
-        defaultValue={String(state?.email)}
       />
 
       <Input
@@ -32,8 +45,11 @@ export function SignIn() {
         legend="Senha"
         type="password"
         placeholder="Digite sua senha"
-        defaultValue={String(state?.password)}
       />
+
+      <p className="text-sm text-red-600 text-center my-4 font-medium">
+        {state?.massage}
+      </p>
 
       <Button type="submit" isloading={isLoading}>
         Entrar
