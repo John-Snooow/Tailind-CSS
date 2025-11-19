c3062087192565236b0f870ad5f0e368fbe4647a
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-11-19 12:48:13 -0300

    Upload do arquivo
    Embora eu tenha concluído Esta vídeo aula Existe um problema Aqui Ao tentar recarregar o arquivo (Imagem) Ela não está sendo salva Na minha pasta de upload No servidor Api.

--- refund/src/pages/Refund.tsx
+++ refund/src/pages/Refund.tsx
@@ -27,7 +27,7 @@ export function Refund() {
   const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [filename, setFilename] = useState<File | null>(null);
+  const [file, setFile] = useState<File | null>(null);
 
   const navigate = useNavigate();
   const params = useParams<{ id: string }>();
@@ -42,6 +42,15 @@ export function Refund() {
     try {
       setIsLoading(true);
 
+      if(!file){
+        return alert("Selecione um arquivo de comprovante");
+      }
+
+      const fileUploadForm = new FormData();
+      fileUploadForm.append("file", file);
+
+      const response = await api.post("/uploads", fileUploadForm);
+
       const data = refundSchema.parse({
         name,
         category,
@@ -50,7 +59,7 @@ export function Refund() {
 
       await api.post("/refunds", {
         ...data,
-        filename: "12345678910111211131415.png"
+        filename: response.data.filename,
       })
 
       navigate("/confirm", { state: { fromSubmit: true } });
@@ -127,8 +136,8 @@ export function Refund() {
         </a>
       ) : (
         <Upload
-          fileName={filename && filename.name}
-          onChange={(e) => e.target.files && setFilename(e.target.files[0])}
+          fileName={file && file.name}
+          onChange={(e) => e.target.files && setFile(e.target.files[0])}
         />
       )}
 
